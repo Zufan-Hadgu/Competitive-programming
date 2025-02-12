173798644d737178e5795022cc821a864f5477a7
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-12 14:21:00 +0300

    Created 'Maximum Ice Cream Bars' by A2SV Hub

---
+++ 12-Feb-2025/Maximum Ice Cream Bars 259410.py
@@ -0,0 +1,16 @@
+# Problem: Maximum Ice Cream Bars - https://leetcode.com/problems/maximum-ice-cream-bars/
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        s = sorted(costs)
+        total = 0
+        count = 0
+        for i in range(len(s)):
+            total += s[i]        
+            if total <= coins:
+                count += 1     
+        return count
+            
+       
+
+        
