c6c11a9ba52966c673148da4efc33f36ca4daf61
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-15 05:35:13 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #1 15-Feb-2025/D - Repeating Cipher 266928.py
@@ -0,0 +1,16 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585132/problem/D
+
+n = int(input())
+enc = input()
+if n == len(enc):
+    x = 0
+    s = []
+    i = 0
+    while i < n and x < n:    
+        s.append(enc[x])
+        i += 1
+        x = x + i 
+    print("".join(s))
+     
+
+   
