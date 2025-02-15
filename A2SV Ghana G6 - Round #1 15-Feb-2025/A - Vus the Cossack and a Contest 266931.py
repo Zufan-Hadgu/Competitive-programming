49cf03342a9ac951dc1d2d35895bbd22729cfa0d
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-15 05:38:13 +0300

    Created 'A - Vus the Cossack and a Contest' by A2SV Hub

---
+++ A2SV Ghana G6 - Round #1 15-Feb-2025/A - Vus the Cossack and a Contest 266931.py
@@ -0,0 +1,13 @@
+# Problem: A - Vus the Cossack and a Contest - https://codeforces.com/gym/585132/problem/A
+
+
+n,k,m= list(map(int,input().split()))
+if n <= k and n <= m:
+    print('Yes')
+else:
+    print('No')
+     
+
+    
+
+ 
