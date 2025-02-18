7e1525813cdd0ae8a25efd9ac89b16889d0ac1f6
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-18 16:23:42 +0300

    Created 'C - Compare T-Shirt Sizes' by A2SV Hub

---
+++ A2SV G6 - Round #1 18-Feb-2025/C - Compare T-Shirt Sizes 271869.py
@@ -0,0 +1,47 @@
+# Problem: C - Compare T-Shirt Sizes - https://codeforces.com/gym/585107/problem/C
+
+n = int(input())
+for i in range(n):
+    n,k = input().split()
+    # print(n,k)
+    len_n = len(n)
+    len_k = len(k)
+    
+    if n == k:
+        print('=')
+        continue
+    if n[len_n-1]=='S' and k[len_k-1] =='S':
+        if len_n > len_k:
+            print('<')
+        elif len_n < len_k:
+            print('>')
+        
+    elif n[len_n-1]=='L' and k[len_k-1] =='L':
+        if len_n > len_k:
+            print('>')
+        elif len_n < len_k:
+            print('<')
+         
+    else:
+        if n[len_n-1]=='L' and k[len_k-1] =='S':
+            print('>')
+
+        elif n[len_n-1]=='S' and k[len_k-1] =='L':
+            print('<')
+        elif n[len_n-1] =='L' and  k[len_k-1] =='M':
+            print('>')
+        elif n[len_n-1] =='M' and  k[len_k-1] =='L':
+            print('<')
+        
+        elif n[len_n-1] =='S' and  k[len_k-1] =='M':
+            print('<')
+        elif n[len_n-1] =='M' and  k[len_k-1] =='S':
+            print('>')
+        
+    
+ 
+
+
+
+        
+
