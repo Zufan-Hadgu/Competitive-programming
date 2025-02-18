db83b062a5755b20b7dc39143931ba7275ce9098
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-18 19:11:09 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 18-Feb-2025/Karen and Coffee 272232.py
@@ -0,0 +1,35 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,q = map(int,input().split())
+diff = [0] * 200002
+m = len(diff)
+
+ 
+for _ in range(n):
+    l,r = map(int,input().split())
+    diff[l] += 1
+    diff[r+1] -= 1
+  
+for i in range(1,m ):
+    diff[i] += diff[i-1]
+for i in range(m):
+    if diff[i] >= k:
+        diff[i] = 1
+    else:
+        diff[i] = 0
+for i in range(1,m):
+    diff[i] += diff[i-1]
+
+for _ in range(q):
+    l, r = map(int, input().split())
+    print(diff[r] - diff[l - 1])  
+
+
+
+
+
+
+
+
+
+
