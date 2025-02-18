cd676b0eda04e65e0bfcee427ea69ec8b6df6b14
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-02-18 11:25:01 +0300

    Created 'Subarray Sum Equals K' by A2SV Hub

---
+++ 18-Feb-2025/Subarray Sum Equals K 270574.py
@@ -0,0 +1,39 @@
+# Problem: Subarray Sum Equals K - https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # acc = 1
+        n = len(nums)
+        prefix = [1] * (n+1)
+        suffix = [1] * (n+1)
+        print(prefix)
+        output = [1]*n
+
+
+        
+        for i in range(n):
+            prefix[i+1] = prefix[i] * nums[i]
+    
+         
+        for i in range(n -2 ,-1,-1):
+            suffix[i] = suffix[i+1] * nums[i+1]
+    
+
+        for i in range(n):
+            output[i] = prefix[i] * suffix[i]
+        return output
+        
+
+        
+
+
+
+        # print(prefix)
+
+         
+
+         
+
+
+
+        
