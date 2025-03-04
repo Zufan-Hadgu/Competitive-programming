854bd11b67414de26efed77acff337678d53ad71
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-04 12:33:18 +0300

    Created 'Red and Blue' by A2SV Hub

---
+++ 04-Mar-2025/Red and Blue 290796.py
@@ -0,0 +1,23 @@
+# Problem: Red and Blue - https://codeforces.com/contest/1469/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    r = list(map(int, input().split()))  
+    m = int(input())
+    b = list(map(int, input().split()))  
+
+    
+    maxr, curr_sum_r = 0, 0
+    for num in r:
+        curr_sum_r += num
+        maxr = max(maxr, curr_sum_r)
+
+     
+    maxb, curr_sum_b = 0, 0
+    for num in b:
+        curr_sum_b += num
+        maxb = max(maxb, curr_sum_b)
+
+ 
+    print(maxr + maxb)
