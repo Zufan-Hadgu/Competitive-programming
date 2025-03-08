3a9209870c77e13ee04c12d5746467d4f1d81e2c
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-08 21:12:13 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 08-Mar-2025/B - Fix the Forecast! 298118.py
@@ -0,0 +1,29 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+from collections import Counter
+
+
+t = int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    b = list(map(int,input().split()))
+    
+    new_a = []
+    output = [0] * len(b)
+
+    for i in range(len(a)):
+        new_a.append((a[i],i))
+
+
+    new_a.sort()
+    b.sort()
+
+    for i in range(n):
+        _,idx = new_a[i] 
+        output[idx] = b[i]
+
+    print(*output)
+
+
+     
