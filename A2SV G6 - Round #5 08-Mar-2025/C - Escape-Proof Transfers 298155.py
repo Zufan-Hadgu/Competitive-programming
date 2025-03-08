84782aae6952b04f6c967150aa8999e551272a3a
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-08 21:49:43 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 08-Mar-2025/C - Escape-Proof Transfers 298155.py
@@ -0,0 +1,28 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+
+n, t, c = map(int, input().split())
+a = list(map(int, input().split()))
+
+l = 0
+r = 0
+possible = 0
+
+while r < n:
+    if a[r] <= t:
+        if r - l + 1 == c:
+            possible += 1
+            l += 1
+            r += 1
+        else:
+            r += 1
+    else:
+        l = r + 1
+        r += 1
+
+print(possible)
+
+     
+    
+            
+    
