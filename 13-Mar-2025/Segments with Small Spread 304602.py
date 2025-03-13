1335bf961ef01deee9d8a5c5a7911d4388a44d9e
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-13 04:56:25 +0300

    Created 'Segments with Small Spread' by A2SV Hub

---
+++ 13-Mar-2025/Segments with Small Spread 304602.py
@@ -0,0 +1,32 @@
+# Problem: Segments with Small Spread - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/F
+
+from collections import deque
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+decr = deque()
+incr = deque()
+l = 0
+output = 0
+
+for r in range(n):
+    while decr and decr[-1] < a[r]:
+        decr.pop()
+    decr.append(a[r])
+
+    while incr and incr[-1] > a[r]:
+        incr.pop()
+    incr.append(a[r])
+
+    while decr[0] - incr[0] > k:
+        value = a[l]
+        if decr[0] == value:
+            decr.popleft()
+        if incr[0] == value:
+            incr.popleft()
+        l += 1
+
+    output += r - l + 1
+
+print(output)
