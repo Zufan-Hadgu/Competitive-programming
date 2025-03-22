a0e6a9b90b66f4ca522e4fc91712edcbd50e1ba6
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-22 20:14:48 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 22-Mar-2025/E - Symmetrization 315705.py
@@ -0,0 +1,27 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+t = int(input())
+for _ in range(t):
+
+    n = int(input())
+    
+    Matrix = []
+    for i in range(n):
+        Matrix.append(input())
+    
+
+    operation = 0
+
+    for row in range(n//2):
+        for col in range(row, n - row - 1):
+            
+            zero_one_count = [0, 0]
+
+            zero_one_count[int(Matrix[row][col])] += 1
+            zero_one_count[int(Matrix[col][n - row - 1])] += 1
+            zero_one_count[int(Matrix[n - row - 1][n - col - 1])] += 1
+            zero_one_count[int(Matrix[n - col - 1][row])] += 1
+
+            operation += min(zero_one_count)
+
+    print(operation)
