45c3fafc12e475dc389f7b6dbe8708f4ec3adb01
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-03-27 22:49:31 +0300

    Created 'Find the Winner of the Circular Game' by A2SV Hub

---
+++ 27-Mar-2025/Find the Winner of the Circular Game 320462.py
@@ -0,0 +1,19 @@
+# Problem: Find the Winner of the Circular Game - https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        queue = deque()
+        for i in range(1,n+1):
+            queue.append(i)
+        while len(queue) > 1:
+            for  i in range(k-1):
+              
+                num = queue.popleft()
+                queue.append(num)
+            queue.popleft()
+            
+        return queue[0]
+       
+                
+            
+        
