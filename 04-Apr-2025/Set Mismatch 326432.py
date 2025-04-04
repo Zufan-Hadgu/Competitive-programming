674bb3b7286048e6167724d06681cde70da15e34
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-04-04 22:36:02 +0300

    Created 'Set Mismatch' by A2SV Hub

---
+++ 04-Apr-2025/Set Mismatch 326432.py
@@ -0,0 +1,20 @@
+# Problem: Set Mismatch - https://leetcode.com/problems/set-mismatch/description/
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        
+        i = 0
+        while i < len(nums):
+            correct_index = nums[i] - 1
+            if nums[i] != nums[correct_index]:
+                nums[i], nums[correct_index] = nums[correct_index], nums[i]
+            else:
+                i += 1
+
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return [nums[i], i + 1]
+
+        return [-1, -1]
+
+        
