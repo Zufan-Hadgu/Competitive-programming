888902c8a6ca057aa11dce1b55417503901dadae
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-05-14 06:57:22 +0300

    Created 'Christmas Spruce' by A2SV Hub

---
+++ 14-May-2025/Christmas Spruce 355011.py
@@ -0,0 +1,34 @@
+# Problem: Christmas Spruce - https://codeforces.com/contest/913/problem/B
+
+from collections import defaultdict, deque
+
+
+n = int(input())
+d = defaultdict(list)
+for  i in range(1,n):
+    v = int(input())
+    d[v].append(i+1)
+q = deque([1])
+flag = True
+
+while q:
+    node = q.popleft()
+    leaf = 0
+
+    for child in d[node]:
+        if child not in d:
+            leaf += 1
+        else:
+            q.append(child)
+    if leaf < 3:
+        flag = False
+        print('No')
+        break
+if flag:
+    print('Yes')
+
+ 
+         
+
+ 
+    
