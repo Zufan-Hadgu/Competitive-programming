2dfccd91e7c34ea168b80d5ac92a64933917e4a1
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-05-29 10:54:52 +0300

    Created 'Maximum Subarray' by A2SV Hub

---
+++ 29-May-2025/Maximum Subarray 360692.py
@@ -0,0 +1,19 @@
+# Problem: Maximum Subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+         
+        result = nums[0]
+        maxend = nums[0]
+        for i in range(1,len(nums)):
+            maxend += nums[i]
+            maxend = max(maxend,nums[i])
+            result = max(maxend,result)
+        return result
+       
+
+
+
+        
+
+        
