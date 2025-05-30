4c9902e192e264065abb9c39d11f751b45b72271
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-05-30 18:12:14 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 30-May-2025/Palindrome Number 361222.py
@@ -0,0 +1,15 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        result = 0
+        temp = x 
+        while x > 0:
+            rem = x % 10
+            result = result * 10 + rem
+            x = x//10
+        return result == temp
+        
+
+
+        
