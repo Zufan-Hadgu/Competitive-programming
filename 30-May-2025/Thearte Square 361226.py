5b73464817992e0eda4ec5986ef67ef1409769e3
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-05-30 18:48:39 +0300

    Created 'Thearte Square' by A2SV Hub

---
+++ 30-May-2025/Thearte Square 361226.py
@@ -0,0 +1,10 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+from math import ceil
+
+
+n,m,a = map(int,input().split())
+length = ceil(n/a)
+height = ceil(m/a)
+
+print(length*height)
