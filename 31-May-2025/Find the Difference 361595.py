e521bcaab20a84bc4423b902f4b3f989866825fe
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-05-31 13:02:50 +0300

    Created 'Find the Difference' by A2SV Hub

---
+++ 31-May-2025/Find the Difference 361595.py
@@ -0,0 +1,12 @@
+# Problem: Find the Difference - https://leetcode.com/problems/find-the-difference/
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        new_list = list(s)
+        for letter in t:
+            if letter in new_list:
+                new_list.remove(letter)
+            else:
+                return letter 
+        
+        
