51bef96950db5f5995469aa442ec5aac46826556
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-06-02 18:32:32 +0300

    Created 'Search Insert Position' by A2SV Hub

---
+++ 02-Jun-2025/Search Insert Position 362807.py
@@ -0,0 +1,13 @@
+# Problem: Search Insert Position - https://leetcode.com/problems/search-insert-position/
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.append(target)
+        nums.sort()
+        return nums.index(target)
+        
