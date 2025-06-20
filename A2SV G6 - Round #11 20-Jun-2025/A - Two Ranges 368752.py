9c06cf408db0b568b05b1205265aee4573d4b9cf
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-06-20 23:17:36 +0300

    Created 'A - Two Ranges' by A2SV Hub

---
+++ A2SV G6 - Round #11 20-Jun-2025/A - Two Ranges 368752.py
@@ -0,0 +1,14 @@
+# Problem: A - Two Ranges - https://codeforces.com/gym/604781/problem/A
+
+q = int(input())
+for _ in range(q):
+    l1,r1,l2,r2 = list(map(int,input().split()))
+    ans = []
+    if l1 != l2:
+        print(l1, l2)
+    else:
+        print(l1, l2 + 1)
+ 
+
+        
+
