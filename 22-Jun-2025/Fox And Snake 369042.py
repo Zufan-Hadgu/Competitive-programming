98cf976aa04532a2ed12e56b16c7d6f8d4719b53
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-06-22 15:20:33 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 22-Jun-2025/Fox And Snake 369042.py
@@ -0,0 +1,19 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n,m = map(int,input().split())
+ans = []
+for i in range(1,n+1):
+
+    if i % 2 != 0:
+        sign = '#' * m
+        ans.append(sign)
+    if i %2 == 0:
+        if (i // 2)  % 2 != 0:
+            sign = ('.'* (m -1)) + '#'
+        else:
+            sign = '#' + ('.' * (m-1))
+        ans.append(sign)
+print('\n'.join(ans))
+
+
+    
