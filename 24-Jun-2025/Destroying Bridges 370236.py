8918483f91cb2fa424b1e0b081082d0012181689
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-06-24 23:32:22 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 24-Jun-2025/Destroying Bridges 370236.py
@@ -0,0 +1,10 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    ans = n
+    for x in range(1, n + 1):
+        if x * (n - x) <= k:
+            ans = min(ans, x)
+    print(ans)
