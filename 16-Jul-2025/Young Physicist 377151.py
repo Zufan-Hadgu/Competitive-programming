01ecdb0660c4dc35f0672029914c6be1a5f73926
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-07-16 23:37:21 +0300

    Created 'Young Physicist' by A2SV Hub

---
+++ 16-Jul-2025/Young Physicist 377151.py
@@ -0,0 +1,14 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+n = int(input())
+sum_m = [0,0,0]
+for _ in range(n):
+    a,b,c = map(int,input().split())
+    sum_m[0] += a
+    sum_m[1] += b
+    sum_m[2] += c
+if sum_m[0] == 0 and sum_m[1] == 0 and sum_m[2] == 0:
+    print("YES")
+else:
+    print("NO")
+
