fcb5d59c775f88022daf7d31c06159d628640d85
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-08-22 00:06:03 +0300

    Created 'Reverse Bits' by A2SV Hub

---
+++ 21-Aug-2025/Reverse Bits 387730.py
@@ -0,0 +1,9 @@
+# Problem: Reverse Bits - https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        binary = format(n, '032b')  
+        reversed_binary = binary[::-1]  
+        return int(reversed_binary, 2) 
+
+      
