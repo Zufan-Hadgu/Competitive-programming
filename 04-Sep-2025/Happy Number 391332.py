1d803902e63200ad0a96062eac9ed78a1be5cfd3
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-09-04 23:25:43 +0300

    Created 'Happy Number' by A2SV Hub

---
+++ 04-Sep-2025/Happy Number 391332.py
@@ -0,0 +1,12 @@
+# Problem: Happy Number - https://leetcode.com/problems/happy-number/description/
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number: int) -> int:
+            return sum(int(digit) ** 2 for digit in str(number))
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
