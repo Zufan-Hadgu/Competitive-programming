1f9dfbf00cfd88b92bf14712d9324a2d12234208
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-09-08 22:54:00 +0300

    Created 'Programmers and Mathematicians' by A2SV Hub

---
+++ 08-Sep-2025/Programmers and Mathematicians 392036.py
@@ -0,0 +1,8 @@
+# Problem: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+t = int(input())
+for _ in range(t):
+    a,b = map(int,input().split())
+    total = a + b
+    team = total // 4
+    print(min(a,b,team))
