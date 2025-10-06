d7f1ce978c76eccd69acc14e36faf1a3b4b27036
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-10-06 19:21:22 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 06-Oct-2025/Almost Prime 400291.py
@@ -0,0 +1,21 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+n = int(input())
+almost = 0
+
+for i in range(2, n + 1):
+    factor = set()
+    num = i
+    d = 2
+    while d * d <= num:
+        while num % d == 0:
+            factor.add(d)
+            num //= d
+        d += 1
+    if num > 1:
+        factor.add(num)
+
+    if len(factor) == 2:
+        almost += 1
+
+print(almost)
