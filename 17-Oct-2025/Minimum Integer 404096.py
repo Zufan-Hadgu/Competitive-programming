22abad4f4166c38c2ae1aa5a276f0a2207d5eab1
Zufan Gebrehiwot <105021433+Zufan-Hadgu@users.noreply.github.com>
2025-10-17 20:19:41 +0300

    Created 'Minimum Integer' by A2SV Hub

---
+++ 17-Oct-2025/Minimum Integer 404096.py
@@ -0,0 +1,12 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+q = int(input())
+for _ in range(q):
+    l,r,d = map(int,input().split())
+    if l > d:
+        print(d)
+    else:
+        n = r // d
+        num = n * d + d
+        print(num)
+
